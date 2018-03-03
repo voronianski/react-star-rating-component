573b808ec00b42e513f779944e7c2f28c989422d
Robert DeSimone <icenine457@gmail.com>
2018-03-03 15:01:46 -0500

    Apply linting to webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
 'use strict';
-const env = process.env.NODE_ENV || "development";
 
-const webpack = require("webpack");
-const path = require("path");
+const env = process.env.NODE_ENV || 'development';
 
-const isProduction = env === "production";
+const path = require('path');
+
+const isProduction = env === 'production';
 
 const PATHS = {
-  root: path.join(__dirname, ""),
-  example: path.join(__dirname, "example"),
+  root: path.join(__dirname, ''),
+  example: path.join(__dirname, 'example')
 };
 
 const config = {
   mode: env,
   entry: {
-    "dist/react-star-rating-component": `./index.jsx`,
-    "example/bundle": `${PATHS.example}/index.jsx`
+    'dist/react-star-rating-component': './index.jsx',
+    'example/bundle': `${PATHS.example}/index.jsx`
   },
 
   output: {
     path: PATHS.root,
-    filename: `[name]${isProduction ? ".min" : ""}.js`,
-    library: "ReactStarRatingComponent",
-    libraryTarget: "umd",
+    filename: `[name]${isProduction ? '.min' : ''}.js`,
+    library: 'ReactStarRatingComponent',
+    libraryTarget: 'umd',
     umdNamedDefine: true
   },
 
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
+    'react': 'React',
+    'react-dom': 'ReactDOM'
   },
 
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
 
   module: {
@@ -40,18 +40,18 @@ const config = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader']
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["eslint-loader"]
+        use: ['eslint-loader']
       }
     ]
   },
   devtool: isProduction
-    ? "cheap-module-source-map"
-    : "cheap-module-eval-source-map"
+    ? 'cheap-module-source-map'
+    : 'cheap-module-eval-source-map'
 };
 
 
